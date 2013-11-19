432cb7f903248598c86921af17f3c5d769830d91
nullren <chopflop@gmail.com>
2013-11-18 19:43:51 -0800

    still get out of memory errors

--- encode.m
+++ encode.m
@@ -1,9 +1,31 @@
-function outFile = encode(imgFile, dataFile)
+function I = encode(imgFileName, dataFileName)
+% if there are errors, we'll let functions throw them then catch them
+% outside of this encode function. or else, we should throw extra
+% errors for things. lol
 
 % read file
-I0 = imread(imgFile);
+I = imread(imgFileName);
 
 % set LSB to 0
-I1 = bitset(I, 1, 0);
+I = bitset(I, 1, 0);
 
+% read payload data
+dataFile = fopen(dataFileName);
+[data, count] = fread(dataFile, Inf, 'uint8');
+fclose(dataFile);
 
+% makesure our payload will fit in our image
+[x, y, channels] = size(I);
+sizeOfSpace = x*y*channels;
+sizeOfPayload = count*8;
+
+if sizeOfPayload > sizeOfSpace
+  error('encode', "payload is too large for this image")
+end
+
+% now put data into a matrix with same dimensions as I1
+dataBin = dec2bin(data, 8)(:) - "0";
+% out of memory
+dataBin = [dataBin ; zeros(sizeOfSpace - size(dataBin, 1))];
+
+I = I + reshape(dataBin, x, y, channels);
